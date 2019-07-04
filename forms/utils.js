17c0504687fbf9a320d4102a1af8ee859e6c0c23
Simon Lydell <simon.lydell@gmail.com>
2019-07-04 16:55:55 +0200

    Updates

--- forms/utils.js
+++ forms/utils.js
@@ -12,35 +12,33 @@ function normalizePath(path) {
 
 
 function fieldFromSchema(schema, field) {
-  var normalizedSchema = {
+  const normalizedSchema = {
     type: "object",
     properties: schema
   };
-  var normalizedPath = normalizePath(field);
-  return normalizedPath.split(".").reduce(function (acc, key) {
+  const normalizedPath = normalizePath(field);
+  return normalizedPath.split(".").reduce((acc, key) => {
     if (acc.type === "object") {
-      var value = acc.properties[key];
+      const value = acc.properties[key];
 
       if (typeof value === "undefined") {
-        throw new Error("Encountered a non-existent key \"".concat(key, "\"."));
+        throw new Error(`Encountered a non-existent key "${key}".`);
       }
 
       return value;
     } else if (acc.type === "array") {
       if (!key.match(/^\d+$/)) {
-        throw new Error("Encountered a non-numeric index \"".concat(key, "\" access on an array."));
+        throw new Error(`Encountered a non-numeric index "${key}" access on an array.`);
       }
 
       return acc.items;
     }
 
-    throw new Error("Encountered lookup \"".concat(key, "\" on unsupported type \"").concat(acc.type, "\"."));
+    throw new Error(`Encountered lookup "${key}" on unsupported type "${acc.type}".`);
   }, normalizedSchema);
 }
 
 function fieldFromErrorResponse(response, field) {
-  var normalizedPath = normalizePath(field);
-  return normalizedPath.split(".").reduce(function (acc, key) {
-    return acc[key.match(/^\d+$/) ? Number(key) : key];
-  }, response);
+  const normalizedPath = normalizePath(field);
+  return normalizedPath.split(".").reduce((acc, key) => acc[key.match(/^\d+$/) ? Number(key) : key], response);
 }
--- utils.js
+++ utils.js
@@ -13,6 +13,7 @@ exports.nthIndexOf = nthIndexOf;
 exports.capitalize = capitalize;
 exports.interpolateString = interpolateString;
 exports.t = t;
+exports.getFromSchema = getFromSchema;
 exports.MultiMeter = exports.ComponentProxy = exports.Translate = void 0;
 
 var _core = require("@material-ui/core");
@@ -25,11 +26,9 @@ var _react = _interopRequireDefault(require("react"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _toArray(arr) { return _arrayWithHoles(arr) || _iterableToArray(arr) || _nonIterableRest(); }
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
 
@@ -43,14 +42,12 @@ function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = 
 
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
-var logger = _jsLogger.default.get("bananas");
+const logger = _jsLogger.default.get("bananas");
 
 function getCookie(name) {
-  var prefix = "".concat(name, "=");
-  var cookies = document.cookie.split(/\s*;\s*/);
-  var match = cookies.find(function (cookie) {
-    return cookie.startsWith(prefix);
-  });
+  const prefix = `${name}=`;
+  const cookies = document.cookie.split(/\s*;\s*/);
+  const match = cookies.find(cookie => cookie.startsWith(prefix));
   return match == null ? undefined : match.slice(prefix.length);
 }
 
@@ -59,10 +56,10 @@ function toQuery(obj) {
     return "";
   }
 
-  var query = Object.keys(obj).map(function (key) {
-    return "".concat(key, "=").concat(encodeURIComponent(obj[key]));
+  const query = Object.keys(obj).map(key => {
+    return `${key}=${encodeURIComponent(obj[key])}`;
   }).join("&");
-  return query ? "?".concat(query) : "";
+  return query ? `?${query}` : "";
 }
 
 function fromQuery(query) {
@@ -70,19 +67,21 @@ function fromQuery(query) {
     return {};
   }
 
-  return query.substring(1).split("&").reduce(function (params, param) {
-    var _param$split = param.split("="),
-        _param$split2 = _slicedToArray(_param$split, 2),
-        key = _param$split2[0],
-        value = _param$split2[1];
+  return query.substring(1).split("&").reduce((params, param) => {
+    const _param$split = param.split("="),
+          _param$split2 = _slicedToArray(_param$split, 2),
+          key = _param$split2[0],
+          value = _param$split2[1];
 
-    return key === "" ? params : _objectSpread({}, params, _defineProperty({}, key, decodeURIComponent(value)));
+    return key === "" ? params : _objectSpread({}, params, {
+      [key]: decodeURIComponent(value)
+    });
   }, {});
 }
 
 function ensureTrailingSlash(path) {
   if (path != null && !path.endsWith("/")) {
-    return "".concat(path, "/");
+    return `${path}/`;
   }
 
   return path;
@@ -90,20 +89,20 @@ function ensureTrailingSlash(path) {
 
 function ensureLeadingHash(hash) {
   if (hash != null && !hash.startsWith("#")) {
-    return "#".concat(hash);
+    return `#${hash}`;
   }
 
   return hash;
 }
 
 function absolutePath(path) {
-  var basename = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "/";
+  let basename = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "/";
 
   if (!path) {
     return path;
   }
 
-  var pathname = path; // Make relative path absolute to basename
+  let pathname = path; // Make relative path absolute to basename
 
   if (!pathname.startsWith("/")) {
     pathname = ensureTrailingSlash(basename) + pathname;
@@ -111,35 +110,15 @@ function absolutePath(path) {
 
 
   if (pathname.indexOf(".") >= 0) {
-    var stack = [];
-    var _iteratorNormalCompletion = true;
-    var _didIteratorError = false;
-    var _iteratorError = undefined;
+    const stack = [];
 
-    try {
-      for (var _iterator = pathname.split("/")[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-        var part = _step.value;
-
-        if (part === ".") {
-          continue;
-        } else if (part === ".." && stack.length > 0) {
-          stack.pop();
-        } else {
-          stack.push(part);
-        }
-      }
-    } catch (err) {
-      _didIteratorError = true;
-      _iteratorError = err;
-    } finally {
-      try {
-        if (!_iteratorNormalCompletion && _iterator.return != null) {
-          _iterator.return();
-        }
-      } finally {
-        if (_didIteratorError) {
-          throw _iteratorError;
-        }
+    for (const part of pathname.split("/")) {
+      if (part === ".") {
+        continue;
+      } else if (part === ".." && stack.length > 0) {
+        stack.pop();
+      } else {
+        stack.push(part);
       }
     }
 
@@ -150,7 +129,7 @@ function absolutePath(path) {
 }
 
 function nthIndexOf(str, pattern, n, start) {
-  var index = str.indexOf(pattern, start || 0);
+  const index = str.indexOf(pattern, start || 0);
 
   if (index >= 0 && n > 1) {
     return nthIndexOf(str, pattern, n - 1, index + 1);
@@ -168,14 +147,12 @@ function capitalize(string) {
 }
 
 function interpolateString(string, params) {
-  return Array.isArray(params) ? params.reduce(function (s, value) {
-    return s.replace(/%[sd]|\{\}/, value);
-  }, string) : Object.entries(params).reduce(function (s, _ref) {
-    var _ref2 = _slicedToArray(_ref, 2),
+  return Array.isArray(params) ? params.reduce((s, value) => s.replace(/%[sd]|\{\}/, value), string) : Object.entries(params).reduce((s, _ref) => {
+    let _ref2 = _slicedToArray(_ref, 2),
         key = _ref2[0],
         value = _ref2[1];
 
-    return s.replace(new RegExp("%\\(".concat(key, "\\)[sd]|\\{").concat(key, "\\}"), "g"), value);
+    return s.replace(new RegExp(`%\\(${key}\\)[sd]|\\{${key}\\}`, "g"), value);
   }, string);
 }
 
@@ -185,12 +162,12 @@ function t(key, params) {
     return key;
   }
 
-  var value = window.i18n[key] || key;
+  const value = window.i18n[key] || key;
   return params ? interpolateString(value, params) : value;
 }
 
-var Translate = function Translate(_ref3) {
-  var children = _ref3.children,
+const Translate = (_ref3) => {
+  let children = _ref3.children,
       params = _ref3.params;
   return _react.default.createElement(_core.Typography, null, t(children, params));
 };
@@ -207,100 +184,88 @@ Translate.defaultProps = {
  * Helper for proxying exposed methods from one or more referenced components.
  */
 
-var ComponentProxy =
-/*#__PURE__*/
-function () {
-  function ComponentProxy() {
-    _classCallCheck(this, ComponentProxy);
-
+class ComponentProxy {
+  constructor() {
     this.proxy = {};
   }
 
-  _createClass(ComponentProxy, [{
-    key: "add",
-    value: function add(Component, alias) {
-      var _this = this;
-
-      var reference = _react.default.createRef(); // Add exposed component actions to proxy
+  add(Component, alias) {
+    const reference = _react.default.createRef(); // Add exposed component actions to proxy
 
 
-      var _iteratorNormalCompletion2 = true;
-      var _didIteratorError2 = false;
-      var _iteratorError2 = undefined;
+    for (const action of Component.expose) {
+      this.proxy[action] = function () {
+        return reference.current ? reference.current[action](...arguments) : null;
+      };
+    } // Remember reference under alias for later use
 
-      try {
-        var _loop = function _loop() {
-          var action = _step2.value;
 
-          _this.proxy[action] = function () {
-            var _reference$current;
-
-            return reference.current ? (_reference$current = reference.current)[action].apply(_reference$current, arguments) : null;
-          };
-        };
-
-        for (var _iterator2 = Component.expose[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-          _loop();
-        } // Remember reference under alias for later use
-
-      } catch (err) {
-        _didIteratorError2 = true;
-        _iteratorError2 = err;
-      } finally {
-        try {
-          if (!_iteratorNormalCompletion2 && _iterator2.return != null) {
-            _iterator2.return();
-          }
-        } finally {
-          if (_didIteratorError2) {
-            throw _iteratorError2;
-          }
-        }
-      }
-
-      if (alias) {
-        this[alias] = reference;
-      }
-
-      return reference;
+    if (alias) {
+      this[alias] = reference;
     }
-  }]);
 
-  return ComponentProxy;
-}();
+    return reference;
+  }
+
+}
 
 exports.ComponentProxy = ComponentProxy;
 
-var MultiMeter = function MultiMeter() {
-  var _this2 = this;
+class MultiMeter {
+  constructor() {
+    _defineProperty(this, "meters", {});
 
-  _classCallCheck(this, MultiMeter);
+    _defineProperty(this, "up", (name, step) => this.change(name, step ? Math.abs(step) : 1));
 
-  _defineProperty(this, "meters", {});
+    _defineProperty(this, "down", (name, step) => this.change(name, step ? -Math.abs(step) : -1));
 
-  _defineProperty(this, "up", function (name, step) {
-    return _this2.change(name, step ? Math.abs(step) : 1);
-  });
+    _defineProperty(this, "step", (up, name) => up ? this.up(name) : this.down(name));
 
-  _defineProperty(this, "down", function (name, step) {
-    return _this2.change(name, step ? -Math.abs(step) : -1);
-  });
-
-  _defineProperty(this, "step", function (up, name) {
-    return up ? _this2.up(name) : _this2.down(name);
-  });
-
-  _defineProperty(this, "change", function (name, delta) {
-    var n = name || "default";
-    _this2.meters[n] = Math.max((_this2.meters[n] || 0) + delta, 0);
-    return _this2.meters[n];
-  });
-
-  _defineProperty(this, "read", function (name) {
-    return name ? _this2.meters[name] > 0 : Object.keys(_this2.meters).some(function (n) {
-      return _this2.reads(n);
+    _defineProperty(this, "change", (name, delta) => {
+      const n = name || "default";
+      this.meters[n] = Math.max((this.meters[n] || 0) + delta, 0);
+      return this.meters[n];
     });
-  });
-};
+
+    _defineProperty(this, "read", name => {
+      return name ? this.meters[name] > 0 : Object.keys(this.meters).some(n => this.reads(n));
+    });
+  }
+
+}
 
 exports.MultiMeter = MultiMeter;
+
+function getFromSchema(schema, path) {
+  return getFromSchemaHelper(schema, path.split("."), []);
+}
+
+function getFromSchemaHelper(schema, pathItems, location) {
+  if (pathItems.length === 0) {
+    return schema;
+  }
+
+  const _pathItems = _toArray(pathItems),
+        key = _pathItems[0],
+        restPath = _pathItems.slice(1);
+
+  const type = {}.toString.call(schema);
+
+  if (type !== "[object Object]") {
+    throw new TypeError(`Cannot access ${JSON.stringify(key)} on ${type} at ${JSON.stringify(["schema", ...location].join("."))}`);
+  }
+
+  if (schema.type === "array") {
+    return getFromSchemaHelper(schema.items, pathItems, [...location, "items"]);
+  }
+
+  if (schema.type === "object") {
+    return getFromSchemaHelper(schema.properties, pathItems, [...location, "properties"]);
+  }
+
+  if (!{}.hasOwnProperty.call(schema, key)) {
+    throw new TypeError(`${JSON.stringify(key)} is not present at ${JSON.stringify(["schema", ...location].join("."))}. Valid choices: ${JSON.stringify(Object.keys(schema))}`);
+  }
+
+  return getFromSchemaHelper(schema[key], restPath, [...location, key]);
+}
